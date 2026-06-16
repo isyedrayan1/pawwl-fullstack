5cc70bffa934c4a048f87f205c2073e873669584
isyedrayan <isyedrayan@gmail.com>
2026-06-16 22:38:24 +0530

    Configure for Hostinger App Deployment

---
+++ server.js
@@ -0,0 +1,4 @@
+// Hostinger Entry Point
+// Hostinger requires a server.js file at the root to start the app.
+// This simply redirects to the compiled backend server.
+import './backend/dist/src/server.js';
